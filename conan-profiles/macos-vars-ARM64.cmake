845b0a63b74e666baa4a8e1052a637afa3fccb61
howetuft <34142192+howetuft@users.noreply.github.com>
2025-10-21 14:48:09 +0200

    Force set CMAKE_OSX_DEPLOYMENT_TARGET and MACOS_VERSION_MIN

--- conan-profiles/macos-vars-ARM64.cmake
+++ conan-profiles/macos-vars-ARM64.cmake
@@ -1,3 +1,4 @@
 # This file injects CMake variables in MacOS build
 # 14.2 is required by opensubdiv for metal
-set(CMAKE_OSX_DEPLOYMENT_TARGET "14.2" CACHE STRING "")
+set(CMAKE_OSX_DEPLOYMENT_TARGET "14.2" CACHE STRING "" FORCE)
+set(MACOS_VERSION_MIN "14.2" CACHE STRING "" FORCE)
