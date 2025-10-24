bdb411f5678801789ff302d8fac7f98953d8db85
howetuft <howetuft@gmail.com>
2025-10-24 22:13:44 +0200

    MacOS ARM - Embree - Unset AVX

--- conan-profiles/macos-vars-ARM64.cmake
+++ conan-profiles/macos-vars-ARM64.cmake
@@ -1,4 +1,7 @@
-# This file injects CMake variables in MacOS build
+# This file injects CMake variables in MacOS ARM build
 # 14.2 is required by opensubdiv for metal
+MESSAGE(STATUS "Injecting Conan CMake variables")
 set(CMAKE_OSX_DEPLOYMENT_TARGET "14.2" CACHE STRING "" FORCE)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
 set(MACOS_VERSION_MIN "14.2" CACHE STRING "" FORCE)
