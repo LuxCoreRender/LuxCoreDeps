9507e546d3f5b3b63f7d47c54ff09d6b964ab4b1
LuxCoreDeps <luxcoredeps@luxcore.com>
2026-04-26 17:18:16 +0200

    Remove debug intermediary conan scripts

--- utils/clean-conan-files.sh
+++ utils/clean-conan-files.sh
@@ -4,10 +4,13 @@ echo "Cleaning Conan intermediary files"
 
 files=(
   "conanbuildenv-release-x86_64.sh" \
+  "conanbuildenv-debug-x86_64.sh" \
   "conanbuild.sh" \
   "conanrunenv-release-x86_64.sh" \
+  "conanrunenv-debug-x86_64.sh" \
   "conanrun.sh" \
   "deactivate_conanbuildenv-release-x86_64.sh" \
+  "deactivate_conanbuildenv-debug-x86_64.sh" \
   "deactivate_conanbuild.sh" \
   "deactivate_conanrun.sh" \
   "graph.json" \
