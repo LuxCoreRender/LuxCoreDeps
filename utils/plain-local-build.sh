46cdbb3d4e915bba254a8d5c1276db5fbc034286
LuxCoreDeps <luxcoredeps@luxcore.com>
2026-05-01 19:39:07 +0200

    Enhance local plain build helper script

--- utils/plain-local-build.sh
+++ utils/plain-local-build.sh
@@ -5,6 +5,19 @@
 # You may also want to clean /tmp/conan-cache, to avoid reinjection of previous
 # builds in the current one
 
+# Specialized for Linux, but could be changed
+export RUNNER_OS=Linux
+export RUNNER_ARCH=X64
+
+
+export LUXDEPS_VERSION=test
+export GCC_VERSION=14   # Note: even for gcc > 14
+export WORKSPACE=.
+export CXX_VERSION=20
+export CMAKE_POLICY_VERSION_MINIMUM=3.25
+export DEPS_BUILD_TYPE=Debug  # Or Release/RelWithDebInfo/MinSizeRel, as needed
+export cache_dir
+
 # Run conan
 export CMAKE_BUILD_PARALLEL_LEVEL=
 source run-conan.sh 2>&1 | tee /tmp/run-conan.log
